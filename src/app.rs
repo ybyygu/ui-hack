cb69a8f9feaba3cbecf91b1ae9df2b36ea434cad
Wenping Guo <ybyygu@gmail.com>
2023-03-20 16:46:33 +0800

    Clean up

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,17 @@
 // [[file:../ui-hack.note::f7df0467][f7df0467]]
 use serde::*;
+
+use egui::Ui;
 // f7df0467 ends here
+
+// [[file:../ui-hack.note::7586f426][7586f426]]
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+enum InputTemplate {
+    Gaussian,
+    Orca,
+    Vasp,
+}
+// 7586f426 ends here
 
 // [[file:../ui-hack.note::9a0316a5][9a0316a5]]
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
@@ -14,8 +25,8 @@ enum Enum {
 #[derive(Deserialize, Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+    // current input generator
+    template: InputTemplate,
 
     title: String,
 
@@ -32,23 +43,23 @@ pub struct TemplateApp {
     #[serde(skip)]
     value: f32,
 }
-// 9a0316a5 ends here
 
-// [[file:../ui-hack.note::e67677fe][e67677fe]]
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            label: "Hello World!".to_owned(),
             value: 2.7,
             radio: Enum::First,
             title: String::new(),
             charge: 0,
             multiplicity: 1,
             theory: String::new(),
+            template: InputTemplate::Orca,
         }
     }
 }
+// 9a0316a5 ends here
 
+// [[file:../ui-hack.note::e67677fe][e67677fe]]
 impl TemplateApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -66,54 +77,34 @@ impl TemplateApp {
 }
 // e67677fe ends here
 
-// [[file:../ui-hack.note::5a6d6884][5a6d6884]]
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
+// [[file:../ui-hack.note::fbd820f2][fbd820f2]]
+fn ui_side_panel(ui: &mut Ui, state: &mut TemplateApp) {
+    ui.heading("Input templates");
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self {
-            label,
-            value,
-            radio,
-            title,
-            charge,
-            multiplicity,
-            theory,
-            ..
-        } = self;
+    ui.vertical(|ui| {
+        ui.selectable_value(&mut state.template, InputTemplate::Vasp, "VASP");
+        ui.selectable_value(&mut state.template, InputTemplate::Orca, "ORCA");
+        ui.selectable_value(&mut state.template, InputTemplate::Gaussian, "Gaussian");
+    });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+    ui.separator();
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ok");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/crates/eframe");
-                    ui.label(".");
-                });
-            });
+    // show egui logo
+    ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+        ui.horizontal(|ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.label("powered by ");
+            ui.hyperlink_to("egui", "https://github.com/emilk/egui");
         });
+    });
+}
+// fbd820f2 ends here
 
-        // The central panel the region left after adding TopPanel's and SidePanel's
-        egui::CentralPanel::default().show(ctx, |ui| {
+// [[file:../ui-hack.note::d0f130e0][d0f130e0]]
+fn ui_central_panel(ui: &mut Ui, state: &mut TemplateApp) {
+    ui.heading(format!("{:?} input generator", state.template));
+    match state.template {
+        InputTemplate::Orca => {
             ui.heading("ORCA input");
 
             // 格线对齐
@@ -123,25 +114,25 @@ impl eframe::App for TemplateApp {
                 .striped(true)
                 .show(ui, |ui| {
                     ui.label("Title:");
-                    ui.add(egui::TextEdit::singleline(title).hint_text("Geom optim"));
+                    ui.add(egui::TextEdit::singleline(&mut state.title).hint_text("Geom optim"));
                     ui.end_row();
                     ui.label("Calculation Type:");
                     egui::ComboBox::from_label("")
-                        .selected_text(format!("{:?}", radio))
+                        .selected_text(format!("{:?}", &mut state.radio))
                         .show_ui(ui, |ui| {
-                            ui.selectable_value(radio, Enum::First, "Single Point");
-                            ui.selectable_value(radio, Enum::Second, "Geometry Optimization");
-                            ui.selectable_value(radio, Enum::Third, "Frequencies");
+                            ui.selectable_value(&mut state.radio, Enum::First, "Single Point");
+                            ui.selectable_value(&mut state.radio, Enum::Second, "Geometry Optimization");
+                            ui.selectable_value(&mut state.radio, Enum::Third, "Frequencies");
                         });
                     ui.end_row();
                     ui.label("Charge:");
-                    ui.add(egui::DragValue::new(charge).speed(1.0));
+                    ui.add(egui::DragValue::new(&mut state.charge).speed(1.0));
                     ui.end_row();
                     ui.label("Multiplicity:");
-                    ui.add(egui::DragValue::new(multiplicity).speed(1.0));
+                    ui.add(egui::DragValue::new(&mut state.multiplicity).speed(1.0));
                     ui.end_row();
                     ui.label("Theory:");
-                    ui.add(egui::TextEdit::singleline(theory).hint_text("B3LYP Def2-SVP"));
+                    ui.add(egui::TextEdit::singleline(&mut state.theory).hint_text("B3LYP Def2-SVP"));
                     ui.end_row();
                 });
 
@@ -158,13 +149,40 @@ impl eframe::App for TemplateApp {
                     .striped(true)
                     .show(ui, |ui| {
                         ui.label("Charge:");
-                        ui.add(egui::DragValue::new(charge).speed(1.0));
+                        ui.add(egui::DragValue::new(&mut state.charge).speed(1.0));
                         ui.end_row();
                         ui.label("RI approximation:");
-                        ui.add(egui::DragValue::new(charge).speed(1.0));
+                        ui.add(egui::DragValue::new(&mut state.charge).speed(1.0));
                         ui.end_row();
                     });
             });
+        }
+        _ => {
+            ui.separator();
+            ui.label("Under Construction!");
+        }
+    }
+}
+// d0f130e0 ends here
+
+// [[file:../ui-hack.note::5a6d6884][5a6d6884]]
+impl eframe::App for TemplateApp {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui_side_panel(ui, self);
+
+        });
+
+        // The central panel the region left after adding TopPanel's and SidePanel's
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui_central_panel(ui, self);
         });
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // [[file:../ui-hack.note::9b1efb1f][9b1efb1f]]
-#![deny(warnings)]
-#![warn(clippy::all, rust_2018_idioms)]
+// #![deny(warnings)]
+// #![warn(clippy::all)]
 // 9b1efb1f ends here
 
 // [[file:../ui-hack.note::57cefa6d][57cefa6d]]
